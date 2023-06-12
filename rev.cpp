530049f4d1f1a4969d6d9320520c8d0c912c9c59
Anushka Dubey <anushkadubey388@gmail.com>
2023-06-12 09:35:51 +0530

    commit

---
+++ rev.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // finding lowercase uppercase and digits
+    /*char ch;
+    cout << ch;
+    cin >> ch;
+    if (islower(ch))
+    {
+        cout << "this is lower case letter" << endl;
+    }
+    else if (isupper(ch))
+    {
+        cout << "This is upper case letter" << endl;
+    }
+    else if (isdigit(ch))
+    {
+        cout <<"This is digit "<<endl;
+    }*/
+
+    // while and for loop
+    /*  int n;
+
+      cin>>n;
+
+  int i=1;
+  while(i<=n){
+      cout<<i<<" ";
+      i++;
+  }
+
+      // for (int i = 0; i <= n; i++)
+      // {
+      //     cout<<i<<" ";
+      //     i++;
+      // }*/
+
+    // sum of 1 to n
+    /*int n;
+    cin >> n;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum= sum +i;
+        i++;
+        cout<< " the sum is "<<sum<<endl;
+    }*/
+
+    // sum of all even numbers
+    /*int n;
+    cin>>n;
+    int sum=0;
+    for(int i=1; i<=n; i++){
+        cout<<"the val of i is "<<i<<endl;
+        cout<<"the value of sum here is "<<sum<<endl;
+        sum = sum + (i * 2);
+        // cout<<"the value of i before "<<i<<endl;
+        i++;
+        cout<<"the value of i after "<<i<<endl;
+        cout<<" the sum is "<<sum<<endl;
+    }*/
+
+    
+}
